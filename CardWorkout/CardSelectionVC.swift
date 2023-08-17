e9ce5c5ca7d3b403bfb521b1026739a2e2bd105e
jonathanmarceloperes <emailmarceloperes@gmail.com>
2023-08-17 14:21:16 -0300

    buttons design updated

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -10,7 +10,6 @@ import UIKit
 class CardSelectionVC: UIViewController {
     
     @IBOutlet var cardImageView: UIImageView!
-    @IBOutlet var buttons: [UIButton]!
     var cards: [UIImage] = Card.allValues
     
     var timer: Timer!
@@ -18,9 +17,6 @@ class CardSelectionVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         startTimer()
-        for button in buttons {
-            button.layer.cornerRadius = 8
-        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
